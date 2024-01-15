cf6cb2562c2cf9c8bcdae1772e9d4632b926cbc3
ZoltanRacz <39461763+ZoltanRacz@users.noreply.github.com>
2024-01-15 13:27:54 +0100

    some comments and deleting Distances from deps

--- src/MomentMatching.jl
+++ src/MomentMatching.jl
@@ -1,36 +1,31 @@
 module MomentMatching
 
-# Write your package code here.
+# Dependencies
+# we might want to specify which functions we need and import only those
 
-using Plots
-using Plots: plot, plot! # importing these functions explicitly avoids bug in vscode underlying all plot functions 
+using Plots # for functions providing figures. Should check if we can avoid via recipes. Calling Plot in a library is not adviced, since people use different back-ends.
+using Plots: plot, plot! # importing these functions explicitly avoids bug in vscode underlying all plot functions. Should check if still needed or has been fixed.
 gr()
-using Plots.PlotMeasures
-using LinearAlgebra
+
+# using LinearAlgebra # sure?
 #using Statistics # let's try if we get away with commenting this out. StatsBase is bigger and calls many functions from Statistics anyways.
-#using StatsBase
-using JLD 
-using Parameters: @with_kw, @unpack
-using DocStringExtensions: FIELDS, TYPEDSIGNATURES, TYPEDEF
-using CSV
-using DataFrames
-#using Interpolations
-using Optim
-using Sobol
-#using FastGaussQuadrature
-#using Roots
-#using SparseArrays
-#using Random
-#using Distributions
-using ProgressMeter
-using Distances: euclidean
+#using StatsBase # sure?
+using JLD # For saving results. We should consider alternatives due to compatibility issues
+using Parameters: @with_kw, @unpack # For keywords in types
+using DocStringExtensions: FIELDS, TYPEDSIGNATURES, TYPEDEF # For easier documentation. Should we use it in the end?
+using CSV # For saving tables as output
+using DataFrames # For dealing with tables
+using Optim # For global phase of estimation
+using Sobol # For global phase of estimation
+using ProgressMeter # For showing progress while running long computations
+
+# exported types and functions
+export estimation, # main function
+       two_stage_estimation
+
 
 include("estimation.jl")
 include("inference.jl")
 include("output.jl")
-
-export estimation,
-       two_stage_estimation
-
 
 end
