ffc0e4a8028550452334bbbfd1e968b33ee1996b
ZoltanRacz <39461763+ZoltanRacz@users.noreply.github.com>
2024-01-24 14:14:53 +0100

    can choose algorithm for local phase

--- src/estimation.jl
+++ src/estimation.jl
@@ -124,7 +124,11 @@ $(FIELDS)
     onlyglo::Bool
     "Logical, true if only local optimization is to be performed"
     onlyloc::Bool
+    "Optimization algorithm used in local stage"
+    local_alg::OptimizationAlgorithm
 end
+
+OptimizationAlgorithm = Union{Optim.AbstractOptimizer,Symbol}
 
 """
 $(TYPEDSIGNATURES)
@@ -133,7 +137,7 @@ Create instance of NumParMM.
 """
 function NumParMM(estset::EstimationSetup; Nglo::T=10000, Nloc::T=100, it::T=10000, onlyglo::Bool=false, onlyloc::Bool=false) where T<:Integer
     full_lb_global, full_ub_global = parambounds(estset.mode)[3:4]
-    return NumParMM(Nglo, Nloc, it, full_lb_global, full_ub_global, onlyglo, onlyloc)
+    return NumParMM(Nglo, Nloc, it, full_lb_global, full_ub_global, onlyglo, onlyloc, NelderMead())
 end
 
 """
@@ -396,7 +400,7 @@ function matchmom(estset::EstimationSetup, pmm::ParMM, npmm::NumParMM, aux::Auxi
             preal = PreallocatedContainers(estset, aux)
             for n in eachindex(chunk)
                 fullind = chunk[n]
-                opt_loc!(objl_ch, xl_ch, moml_ch, momnorml_ch, conv_ch, estset, npmm.it, aux, presh, preal, pmm, xsort[fullind], n)
+                opt_loc!(objl_ch, xl_ch, moml_ch, momnorml_ch, conv_ch, npmm.local_alg, estset, npmm.it, aux, presh, preal, pmm, xsort[fullind], n)
                 objf!(moml_ch[n], momnorml_ch[n], estset, xl_ch[n], pmm, aux, presh, preal)
                 ProgressMeter.next!(prog)
             end
@@ -434,11 +438,42 @@ $(TYPEDSIGNATURES)
 
 Perform estimation routine, local stage.
 """
-function opt_loc!(obj::Vector{Float64}, xsol::Vector{Vector{Float64}}, mom::Vector{Vector{Float64}}, momnorm::Vector{Vector{Float64}}, conv::Vector{Bool}, estset::EstimationSetup, iter::Integer, aux::AuxiliaryParameters, presh::PredrawnShocks, preal::PreallocatedContainers, pmm::ParMM, xcand::Vector{Float64}, n::Int64)
-    sol = optimize(y -> objf!(mom[n], momnorm[n], estset, y, pmm, aux, presh, preal), xcand, NelderMead(), Optim.Options(iterations=iter, store_trace=true, g_tol=10^-12))
+function opt_loc!(obj::Vector{Float64}, xsol::Vector{Vector{Float64}}, mom::Vector{Vector{Float64}}, momnorm::Vector{Vector{Float64}}, conv::Vector{Bool}, local_alg::Optim.AbstractOptimizer,estset::EstimationSetup, iter::Integer, aux::AuxiliaryParameters, presh::PredrawnShocks, preal::PreallocatedContainers, pmm::ParMM, xcand::Vector{Float64}, n::Int64)
+    sol = optimize(y -> objf!(mom[n], momnorm[n], estset, y, pmm, aux, presh, preal), xcand, local_alg, Optim.Options(iterations=iter, store_trace=true, g_tol=10^-12))
     obj[n] = Optim.minimum(sol)
     xsol[n] = Optim.minimizer(sol)
     conv[n] = Optim.converged(sol)
+    return nothing
+end
+
+function opt_loc!(obj::Vector{Float64}, xsol::Vector{Vector{Float64}}, mom::Vector{Vector{Float64}}, momnorm::Vector{Vector{Float64}}, conv::Vector{Bool}, local_alg::Symbol,estset::EstimationSetup, iter::Integer, aux::AuxiliaryParameters, presh::PredrawnShocks, preal::PreallocatedContainers, pmm::ParMM, xcand::Vector{Float64}, n::Int64)
+
+    opt = Opt(local_alg, length(xcand)) # or LN_COBYLA
+    function funf(x::Vector, grad::Vector)
+        if length(grad) > 0
+        end
+        return objf!(mom[n], momnorm[n], estset, x, pmm, aux, presh, preal)
+    end
+    
+    opt.min_objective = funf
+    
+    opt.lower_bounds = pmm.lb_hard
+    opt.upper_bounds = pmm.ub_hard
+    
+    opt.maxeval = iter
+    
+    # ? opt.initial_step = [0.01,0.002,0.1,0.01,0.001,0.01,0.005,0.05]
+    
+    (optf,optx,ret) = NLopt.optimize(opt, xcand)
+
+    obj[n] = optf
+    xsol[n] = optx
+    if ret == :SUCCESS || ret == :FTOL_REACHED || ret == :XTOL_REACHED
+        conv[n] = true
+    else
+        conv[n] = false
+    end
+
     return nothing
 end
 
