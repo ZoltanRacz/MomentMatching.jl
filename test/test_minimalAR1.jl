7365ee354d533e280a9551813a38dbdc44a22acc
ZoltanRacz <39461763+ZoltanRacz@users.noreply.github.com>
2024-01-30 12:22:29 +0100

    add necessary dependency

--- test/test_minimalAR1.jl
+++ test/test_minimalAR1.jl
@@ -4,7 +4,7 @@ include("examples/minimalAR1.jl")
 
 setup = EstimationSetup(AR1Estimation("ar1estim"),"","")
 
-npest = NumParMM(setup; Nglo=1000, Nloc=10, it = 3000)
+npest = NumParMM(setup; Nglo=100, Nloc=10, it = 3000)
 
 Tdis = 100
 Ndata = 30000
@@ -19,5 +19,5 @@ preal = PreallocatedContainers(AR1Estimation("ar1estim"),"","",aux)
 
 MomentMatching.obj_mom!(fill(0.0,3), fill(0.0,3), AR1Estimation("ar1estim"), [0.9,0.2,0.1], "", "", aux, presh, preal)
 
-est_1st = estimation(setup; npmm=npest)
+est_1st = estimation(setup; npmm=npest, saving = false)
 boot_1st = param_bootstrap_result(setup, est_1st,auxmomsim, Nseed, Nsample, Ndata, saving = false)
