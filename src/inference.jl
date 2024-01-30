915a7427d8b953f5449fd4f933dc2711809242cd
ZoltanRacz <39461763+ZoltanRacz@users.noreply.github.com>
2024-01-30 15:07:21 +0100

    simplify

--- src/inference.jl
+++ src/inference.jl
@@ -153,7 +153,7 @@ function param_bootstrap(estset::EstimationSetup, mmsolu::EstimationResult,auxmo
                 sample_i = CartesianIndices((Nseeds,Nsamplesim))[fullind][2]
                 x = [[1.0]]
 
-                opt_loc!([1.0], x, [Vector{Float64}(undef,momleng)], [Vector{Float64}(undef,momleng)], [false], mmsolu.npmm.local_alg, estset, npmm.it, aux, presh_repeat[seed_i], preal, mms[sample_i], bestx, 1)
+                opt_loc!([1.0], x, [Vector{Float64}(undef,momleng)], [Vector{Float64}(undef,momleng)], [false], npmm.local_alg, estset, npmm.it, aux, presh_repeat[seed_i], preal, mms[sample_i], bestx, 1)
 
                 x_ch[n] = x[1]
                 ProgressMeter.next!(prog)
