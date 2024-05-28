2841eb9fc068ed4c14b97d62837b192a48c48996
gualtiazza <gualtiero.azzalini@gmail.com>
2024-05-28 16:47:33 +0200

    keep writing docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,6 @@
 using Documenter, MomentMatching
 
-makedocs(sitename="MomentMatching.jl")
+makedocs(sitename="MomentMatching.jl",
+        pages = ["Home" => "index.md",
+                "Manual" => "manual.md",
+                "Examples" => "examples.md"])
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,1 +1,5 @@
-## Prova
+# MomentMatching.jl 
+
+*by Gualtiero Azzalini (Stockholm School of Economics) and Zoltán Rácz (Stockholm School of Economics)*
+
+This package allows to perform Method of Moments estimation (Generalized and Simulated).
