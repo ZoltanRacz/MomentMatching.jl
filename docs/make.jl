2f1fdcb518a77e811c268a78fb06c7b2ca985628
ZoltanRacz <zoltanracz.49@gmail.com>
2025-04-26 18:59:24 +0200

    comment out empty pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,13 @@
 using Documenter, MomentMatching
 
+#=
 makedocs(sitename="MomentMatching.jl",
         pages = ["Home" => "index.md",
                 "Estimation" => "estimation.md",
                 "Inference and Diagnostics" => "inference.md",
                 "Output" => "output.md",
-                "Examples" => "examples.md"])
+                "Examples" => "examples.md"]) =#
+
+makedocs(sitename="MomentMatching.jl",
+                pages = ["Home" => "index.md",
+                "Example" => "examples.md"])
